445a6fec6a131ed4411ef0cf5e7c237cc232053a
Bogdan Sachelarie <bogdan.sachelarie@gmail.com>
2016-05-10 00:21:35 +0300

    Absolute paths & multiple tasks dirs

--- env/buildEnv.sh
+++ env/buildEnv.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-ENV_PATH=$(dirname "$0")
+ENV_PATH=$(readlink -f $(dirname "$0"))
 TEMPLATE_PATH="$ENV_PATH/templates"
 
 function initTaskDir {
-    TASK_NAME=${1:?buildTaskDir <TASK_NAME>}
-
-    mkdir "$TASK_NAME"
-    cd "$TASK_NAME"
-    cp "$TEMPLATE_PATH/taskSolver.py" "$TASK_NAME.py"
-    chmod u+x "$TASK_NAME.py"
+    for TASK_NAME in "$@"
+    do
+        mkdir "$TASK_NAME"
+        cp "$TEMPLATE_PATH/taskSolver.py" "$TASK_NAME/$TASK_NAME.py"
+    done
 }
 
 cat << EOF
 CodeJam environment:
-    [*] initTaskDir <TASK_NAME>
+    [*] initTaskDir <TASK_NAME_0>...
 EOF
