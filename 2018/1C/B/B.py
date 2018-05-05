9ea1f95dcd69f6c810a7d747926403dac2c8a51d
Bogdan Sachelarie <bogdan.sachelarie@gmail.com>
2018-05-06 00:31:55 +0300

    CJ / 2018 / 1C - B Interactive flush @

--- 2018/1C/B/B.py
+++ 2018/1C/B/B.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
-import math, collections, itertools
-from sys import stdin
+import collections, itertools, math
+from collections import Counter, defaultdict, deque, namedtuple, OrderedDict
+from itertools import count, cycle, repeat, groupby, tee, zip_longest
+from itertools import product, permutations, combinations, combinations_with_replacement
+from sys import stderr, stdin, stdout
 
 
 def readValue(valueType):
     return valueType(stdin.readline())
 
-
 def readValues(valueType):
     return list(map(valueType, stdin.readline().split()))
 
+def flushPrintln(line, fout=stdout):
+    println(line, fout)
+    fout.flush()
+
+def flushPrintlns(lines, fout=stdout):
+    for line in lines:
+        println(line, fout)
+    fout.flush()
+
+def println(line, fout=stdout):
+    if isinstance(line, str):
+        print(line, file=fout)
+    elif isinstance(line, collections.Iterable):
+        print(' '.join(map(str, line)), file=fout)
+    else:
+        print(line, file=fout)
 
 class Mouth():
     count = 1
@@ -19,16 +37,38 @@ class Mouth():
         print("Case #{}: {}".format(cls.count, answer))
         cls.count += 1
 
-
 def readInput():
-    read = None
-    return read
+    return readValue(int)
 
+def solve(args):
+    N = args
+    counts = [1] * N
+    available = set(range(N))
 
-def solve(*args, **kwargs):
-    pass
+    def prob(i):
+        return counts[i] / sum(counts)
 
+    for _ in range(N):
+        prefs = readValues(int)
+        if prefs == [-1]:
+            return False
+        prefs = prefs[1:]
+
+        for p in prefs:
+            counts[p] += 1
+
+        prefs = [p for p in prefs if p in available]
+        if not prefs:
+            flushPrintln(-1)
+        else:
+            choice = min(prefs, key=prob)
+            flushPrintln(choice)
+            available.remove(choice)
+
+    return True
 
 if __name__ == '__main__':
     for _ in range(readValue(int)):
-        Mouth.answer(solve(readInput()))
+        ok = solve(readInput())
+        if not ok:
+            break
--- env/res/cjSolver.py
+++ env/res/cjSolver.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
-import math, collections, itertools
-from sys import stdin
+import collections, itertools, math
+from collections import Counter, defaultdict, deque, namedtuple, OrderedDict
+from itertools import count, cycle, repeat, groupby, tee, zip_longest
+from itertools import product, permutations, combinations, combinations_with_replacement
+from sys import stderr, stdin, stdout
 
 
 def readValue(valueType):
     return valueType(stdin.readline())
 
-
 def readValues(valueType):
     return list(map(valueType, stdin.readline().split()))
 
+def flushPrintln(line, fout=stdout):
+    println(line, fout)
+    fout.flush()
+
+def flushPrintlns(lines, fout=stdout):
+    for line in lines:
+        println(line, fout)
+    fout.flush()
+
+def println(line, fout=stdout):
+    if isinstance(line, str):
+        print(line, file=fout)
+    elif isinstance(line, collections.Iterable):
+        print(' '.join(map(str, line)), file=fout)
+    else:
+        print(line, file=fout)
 
 class Mouth():
     count = 1
@@ -19,15 +37,12 @@ class Mouth():
         print("Case #{}: {}".format(cls.count, answer))
         cls.count += 1
 
-
 def readInput():
     read = _
     return read
 
-
 def solve(*args):
     _
-
 
 if __name__ == '__main__':
     for _ in range(readValue(int)):
